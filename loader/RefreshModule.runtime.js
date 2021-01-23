803d5e08833c6d596acbca14ce3a73a73ffd9fb2
Jeff Huijsmans <jeff@kroot.dev>
2021-01-23 21:32:45 +0100

    babel-ize runtime js for ios 9 support (no const)

--- loader/RefreshModule.runtime.js
+++ loader/RefreshModule.runtime.js
@@ -11,71 +11,60 @@
  * [Reference for HMR Error Recovery](https://github.com/webpack/webpack/issues/418#issuecomment-490296365)
  */
 module.exports = function () {
-  const currentExports = __react_refresh_utils__.getModuleExports(module.id);
+  var currentExports = __react_refresh_utils__.getModuleExports(module.id);
+
   __react_refresh_utils__.registerExportsForReactRefresh(currentExports, module.id);
 
   if (module.hot) {
-    const isHotUpdate = !!module.hot.data;
-    const prevExports = isHotUpdate ? module.hot.data.prevExports : null;
+    var isHotUpdate = !!module.hot.data;
+    var prevExports = isHotUpdate ? module.hot.data.prevExports : null;
 
     if (__react_refresh_utils__.isReactRefreshBoundary(currentExports)) {
       module.hot.dispose(
-        /**
-         * A callback to performs a full refresh if React has unrecoverable errors,
-         * and also caches the to-be-disposed module.
-         * @param {*} data A hot module data object from Webpack HMR.
-         * @returns {void}
-         */
-        function hotDisposeCallback(data) {
-          // We have to mutate the data object to get data registered and cached
-          data.prevExports = currentExports;
-        }
-      );
+      /**
+       * A callback to performs a full refresh if React has unrecoverable errors,
+       * and also caches the to-be-disposed module.
+       * @param {*} data A hot module data object from Webpack HMR.
+       * @returns {void}
+       */
+      function hotDisposeCallback(data) {
+        // We have to mutate the data object to get data registered and cached
+        data.prevExports = currentExports;
+      });
       module.hot.accept(
-        /**
-         * An error handler to allow self-recovering behaviours.
-         * @param {Error} error An error occurred during evaluation of a module.
-         * @returns {void}
-         */
-        function hotErrorHandler(error) {
-          if (
-            typeof __react_refresh_error_overlay__ !== 'undefined' &&
-            __react_refresh_error_overlay__
-          ) {
-            __react_refresh_error_overlay__.handleRuntimeError(error);
-          }
-
-          if (typeof __react_refresh_test__ !== 'undefined' && __react_refresh_test__) {
-            if (window.onHotAcceptError) {
-              window.onHotAcceptError(error.message);
-            }
-          }
-
-          __webpack_require__.c[module.id].hot.accept(hotErrorHandler);
+      /**
+       * An error handler to allow self-recovering behaviours.
+       * @param {Error} error An error occurred during evaluation of a module.
+       * @returns {void}
+       */
+      function hotErrorHandler(error) {
+        if (typeof __react_refresh_error_overlay__ !== 'undefined' && __react_refresh_error_overlay__) {
+          __react_refresh_error_overlay__.handleRuntimeError(error);
         }
-      );
+
+        if (typeof __react_refresh_test__ !== 'undefined' && __react_refresh_test__) {
+          if (window.onHotAcceptError) {
+            window.onHotAcceptError(error.message);
+          }
+        }
+
+        __webpack_require__.c[module.id].hot.accept(hotErrorHandler);
+      });
 
       if (isHotUpdate) {
-        if (
-          __react_refresh_utils__.isReactRefreshBoundary(prevExports) &&
-          __react_refresh_utils__.shouldInvalidateReactRefreshBoundary(prevExports, currentExports)
-        ) {
+        if (__react_refresh_utils__.isReactRefreshBoundary(prevExports) && __react_refresh_utils__.shouldInvalidateReactRefreshBoundary(prevExports, currentExports)) {
           module.hot.invalidate();
         } else {
           __react_refresh_utils__.enqueueUpdate(
-            /**
-             * A function to dismiss the error overlay after performing React refresh.
-             * @returns {void}
-             */
-            function updateCallback() {
-              if (
-                typeof __react_refresh_error_overlay__ !== 'undefined' &&
-                __react_refresh_error_overlay__
-              ) {
-                __react_refresh_error_overlay__.clearRuntimeErrors();
-              }
+          /**
+           * A function to dismiss the error overlay after performing React refresh.
+           * @returns {void}
+           */
+          function updateCallback() {
+            if (typeof __react_refresh_error_overlay__ !== 'undefined' && __react_refresh_error_overlay__) {
+              __react_refresh_error_overlay__.clearRuntimeErrors();
             }
-          );
+          });
         }
       }
     } else {
